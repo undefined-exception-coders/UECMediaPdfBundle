955fe6f15fda6566253b8cb075f0cc732d9c8439
Damiano Ciarla <damiano.ciarla@gmail.com>
2016-02-28 09:15:48 +0100

    OptionsResolverInterface in OptionsResolver

--- Form/Type/MediaPdfFormType.php
+++ Form/Type/MediaPdfFormType.php
@@ -4,7 +4,7 @@ namespace UEC\MediaPdfBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MediaPdfFormType extends AbstractType
 {
@@ -25,7 +25,7 @@ class MediaPdfFormType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolverInterface $resolver)
+    public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => $this->modelClass,
