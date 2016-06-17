8c696a42a2ff5254b410c96bc44e22e30f96bc3d
Damiano Ciarla <damiano.ciarla@gmail.com>
2016-06-17 09:22:58 +0200

    configureOptions to setDefaultOptions

--- Form/Type/MediaPdfFormType.php
+++ Form/Type/MediaPdfFormType.php
@@ -4,7 +4,7 @@ namespace UEC\MediaPdfBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class MediaPdfFormType extends AbstractType
 {
@@ -25,7 +25,7 @@ class MediaPdfFormType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
             'data_class' => $this->modelClass,
